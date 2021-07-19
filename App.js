5d73e178d6cd3fd249676f563e8479509ca566d9
Woojin <sam4woojin@naver.com>
2021-07-19 23:39:24 +0900

    Cache Images & Fonts

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import { Text, Image } from "react-native";
+import { Asset } from "expo-asset";
+import { Ionicons } from "@expo/vector-icons";
+
+const cacheImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      Asset.fromModule(image).downloadAsync();
+    }
+  });
+
+const cacheFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [isReady, setIsReady] = useState(false);
+  const loadAssets = () => {
+    const images = cacheImages([
+      "https://images.unsplash.com/photo-1626631047919-3ebbbb76ddf3?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+      require("./assets/splash.png"),
+    ]);
+
+    const fonts = cacheFonts([Ionicons.font]);
+    // console.log(images);
+    // console.log(fonts);
+    return Promise.all([...images, ...fonts]);
+  };
+
+  const onFinish = () => setIsReady(true);
+
+  return isReady ? (
+    <Text>Ready!</Text>
+  ) : (
+    <AppLoading
+      startAsync={loadAssets}
+      onFinish={onFinish}
+      onError={console.error}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
