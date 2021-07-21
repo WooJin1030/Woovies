dadba02820d24b3ac1442554c2eba81555cc7f25
Woojin <sam4woojin@naver.com>
2021-07-21 23:35:39 +0900

    Get API

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, Button } from "react-native";
+import { movieApi } from "../api";
 
-export default ({ navigation }) => (
-  <View style={{ flex: 1, backgroundColor: "black" }}>
-    <Text>Movies</Text>
-    <Button title="Detail" onPress={() => navigation.navigate("Detail")} />
-  </View>
-);
+export default () => {
+  const [nowPlaying, setNowPlaying] = useState({
+    movies: [],
+    error: null,
+  });
+
+  const getData = async () => {
+    const [nowPlaying, error] = await movieApi.nowPlaying();
+    const [popular, popularError] = await movieApi.popular();
+    const [upcoming, upcomingError] = await movieApi.upcoming();
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <View style={{ flex: 1, backgroundColor: "black" }}>
+      <Text>Movies</Text>
+    </View>
+  );
+};
