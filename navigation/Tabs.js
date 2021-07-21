592ab0d9f4a3450462e59cb9c455310d89370a67
Woojin <sam4woojin@naver.com>
2021-07-21 22:19:19 +0900

    Bottom Tab Navigator Styling

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,10 +1,12 @@
 import React, { useLayoutEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
 import Favs from "../screens/Favs";
 import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
+import { Platform } from "react-native";
 
 const Tabs = createBottomTabNavigator();
 
@@ -21,7 +23,39 @@ export default ({ navigation, route }) => {
   }, [route]);
 
   return (
-    <Tabs.Navigator>
+    <Tabs.Navigator
+      screenOptions={({ route }) => {
+        let iconName = Platform.OS === "ios" ? "ios-" : "md-";
+        if (route.name === "Movies") {
+          iconName += "film";
+        } else if (route.name === "Tv") {
+          iconName += "tv";
+        } else if (route.name === "Search") {
+          iconName += "search";
+        } else if (route.name === "Favourites") {
+          iconName += "heart";
+        }
+
+        return {
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Ionicons
+                name={iconName}
+                color={focused ? "white" : "grey"}
+                size={26}
+              />
+            );
+          },
+        };
+      }}
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          backgroundColor: "black",
+          borderTopColor: "black",
+        },
+      }}
+    >
       <Tabs.Screen name="Movies" component={Movies} />
       <Tabs.Screen name="Tv" component={Tv} />
       <Tabs.Screen name="Search" component={Search} />
