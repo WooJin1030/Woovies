1e8ca7d305df9d3c99ac26b64532a1227c2d21ac
Woojin <sam4woojin@naver.com>
2021-08-24 22:58:58 +0900

    Slider Info

--- components/Movies/Slide.js
+++ components/Movies/Slide.js
@@ -1,28 +1,80 @@
 import React from "react";
 import styled from "styled-components/native";
 import PropTypes from "prop-types";
-import { Dimensions, Image } from "react-native";
 import { apiImage } from "../../api";
-
-const { width: WIDTH, height: HEIGHT } = Dimensions.get("screen");
+import Poster from "../Poster";
+import { TouchableOpacity } from "react-native";
 
 const Container = styled.View`
-  width: ${WIDTH}px;
-  height: ${HEIGHT / 4}px;
-  background-color: red;
+  height: 100%;
+  width: 100%;
 `;
 
 const BG = styled.Image`
   height: 100%;
   width: 100%;
+  opacity: 0.4;
+  position: absolute;
 `;
 
-const Slide = ({ id, title, backgroundImage, votes, overview }) => (
+const Content = styled.View`
+  height: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const Data = styled.View`
+  width: 50%;
+  align-items: flex-start;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  margin-bottom: 10px;
+`;
+
+const Votes = styled.Text`
+  color: rgb(220, 220, 220);
+  margin-bottom: 7px;
+  font-size: 12px;
+`;
+
+const Overview = styled.Text`
+  color: rgb(220, 220, 220);
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const Button = styled.View`
+  margin-top: 10px;
+  background-color: #e74c3c;
+  padding: 5px 10px;
+  border-radius: 3px;
+`;
+
+const ButtonText = styled.Text`
+  color: white;
+`;
+
+const Slide = ({ id, title, backgroundImage, votes, overview, poster }) => (
   <Container>
-    <BG
-      style={{ width: "100%", height: "100%" }}
-      source={{ uri: apiImage(backgroundImage) }}
-    />
+    <BG resizeMode="cover" source={{ uri: apiImage(backgroundImage) }} />
+    <Content>
+      <Poster url={apiImage(poster)} />
+      <Data>
+        <Title>{title.slice(0, 20)}</Title>
+        <Votes>⭐ {votes} /10</Votes>
+        <Overview>{overview.slice(0, 120)}</Overview>
+        <TouchableOpacity>
+          <Button>
+            <ButtonText>View details</ButtonText>
+          </Button>
+        </TouchableOpacity>
+      </Data>
+    </Content>
   </Container>
 );
 
